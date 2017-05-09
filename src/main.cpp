7335e7d75f142a21f5903aa321008ff180181ce9
Negi <SP00KYB00G13@users.noreply.github.com>
2017-05-09 13:46:56 -0400

    updating terminally ill syntax

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,21 @@ int main()
     chain *ch = newChain();
     uint32_t size = 1000;
 
-    for (uint32_t i = 0; i < size; i++)
-    {
+    for (uint32_t i = 0; i < size; i++) {
         uint64_t key = 0xFFFF0000FFFF0000 + i;
         uint32_t len = 4;
         pack **packs = (pack **)malloc(sizeof(pack *) * len);
-        if (packs == NULL) break;
-        for (uint32_t j = 0; j < len; j++)
-        {
+        if (packs == NULL)
+            break;
+        for (uint32_t j = 0; j < len; j++) {
             packs[j] = newPack("dn:testtesttesttesttesttesttesttest",
                                4*1024*1024,
                                "xt:testtesttesttesttesttesttesttest",
                                "tr:testtesttesttesttesttesttesttest");
         }
         
-        if (!insertBlock(newBlock(key, len, packs), ch)) break;
+        if (!insertBlock(newBlock(key, len, packs), ch))
+            break;
     }
     printBlock(ch->head[0]);
     printBlock(ch->head[size - 1]);
