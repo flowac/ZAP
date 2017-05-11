bd43d3224a6c3840a36328b0ba1d09c010e8356e
Gator Developer <gator@ubuntu.(none)>
2017-05-11 13:02:33 -0400

    created ssl fn that creates sha1 from a path

---
+++ include/ssl_fn.h
@@ -0,0 +1,18 @@
+#ifndef _SSL_FN_H
+#define _SSL_FN_H
+
+/* String buffer size when reading from file */
+#define file_input_buff_size 512
+
+/* Create the sha1sum of the file pointed to by dst
+ * DESCRIPTION:
+ * Take an absolute path and calculate the sha1sum of this path
+ * Gonna need something like this to make the xt URN
+ * INPUT:
+ * char *dst - string with an absolute path
+ * RETURN:
+ * NULL - failure
+ */
+unsigned char *create_sha1sum(char *dst);
+
+#endif //_SS_FN_H
---
+++ src/ssl_fn.cpp
@@ -0,0 +1,43 @@
+/* YO @Flowing-water we gotta start plopping GPL all up in here */
+   
+#include "ssl_fn.h"
+#include <openssl/sha.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char *create_sha1sum(char *dst)
+{
+    SHA_CTX ctx; // sha1 struct (look at sha.h)
+    unsigned char *sha1sum = NULL; // sha1sum dest
+    char buffer[file_input_buff_size]; // buffer for file i/o
+    FILE *p_dst = NULL; // fd to dst
+    
+    p_dst = fopen(dst, "r");
+    if (!p_dst) {
+	/* we gonna have a log fn? */
+	perror("fopen");
+	goto end;
+    }
+
+    // wtf spartan naming and camel case? nice libssl
+    if (!SHA1_Init(&ctx))// init the struct
+	goto end; // ssl calls return 0 on fail
+
+    // read file in
+    while (fgets(buffer, file_input_buff_size, p_dst)) {
+	// update teh sha1sum with what we read
+	if(!SHA1_Update(&ctx,
+			(void*)buffer,
+			strlen(buffer))) // u like this syntax @ flowing? u mad bro?
+	    goto end;
+    }
+    // create hash
+    sha1sum = (unsigned char*)malloc(sizeof(char) * SHA_DIGEST_LENGTH);
+    if (!sha1sum)
+	goto end;
+    SHA1_Final(sha1sum, &ctx);
+
+ end:
+    return sha1sum;
+}
