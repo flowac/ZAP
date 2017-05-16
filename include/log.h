82769e6414afbc4e4c4c2faa0bcaab71261ef47d
negi <hnmurz@gmail.com>
2017-05-16 18:59:29 -0400

    added fn comment for log_msg

--- include/log.h
+++ include/log.h
@@ -4,6 +4,13 @@
 #ifndef _LOG_H
 #define _LOG_H
 
+/* This function will append to the log file
+ * INPUT:
+ * char *msg - message to be printed, can be formatted with %s etc
+ * RETURN:
+ * 0 - failure
+ * 1 - success
+ */
 int log_msg(char const *msg, ...);
 
 #endif // _LOG_H
