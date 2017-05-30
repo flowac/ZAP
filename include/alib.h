785a1ee2d69f076f09c6ef670d183b1e83c5e090
Negi <hnmurz@gmail.com>
2017-05-30 13:50:46 -0400

    adding missing semicolon

--- include/alib.h
+++ include/alib.h
@@ -62,7 +62,7 @@ uint32_t deleteChain(chain *target);
 
 bool chainToText(chain *ch, FILE *fp);
 
-bool chainCompactor(chain *ch, uint8_t parts = 1)
+bool chainCompactor(chain *ch, uint8_t parts = 1);
 
 #endif//_ALIB_H
 
