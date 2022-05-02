8f51f3f5bf787c60ff0b3f95185a95d5f6311308
Alan <alanalt5@live.ca>
2022-05-01 20:46:10 -0400

    Added a pirate bay scraper. This took a lot longer than expected because couldn't get a JS enabled webpage downloader to work. Right now you will have to download pages manually.

---
+++ extern/scrap/fetch.py
@@ -0,0 +1,28 @@
+import bs4
+import re
+
+fin = open('pirate.html', 'r')
+fout = open('pirate.txt', 'w')
+html = fin.read()
+fin.close()
+
+root=bs4.BeautifulSoup(html, 'lxml')
+
+for idx, item in enumerate(root.findAll(id='st')):
+    buf = ''
+    txt = item.decode_contents()
+    size = re.search('value=\"(\d+)', txt).group(1)
+    detail = item.findAll('a')
+    extra = item.findAll('span')
+    
+    buf += '\nidx:' + str(idx) + '\nsize:' + size
+    buf += '\nmajor:' + detail[0].string
+    buf += '\nminor:' + detail[1].string
+    buf += '\nname:' + detail[2].string
+    buf += '\ndate:' + extra[2].string
+    buf += '\nseeds:' + extra[5].string
+    buf += '\nleech:' + extra[6].string
+    buf += '\n' + detail[3]['href'] + '\n'
+    fout.write(buf)
+fout.close()
+
